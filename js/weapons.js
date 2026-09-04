4281f6ec95ed3fdd33fab264a84313647e342266
StormRat115 <Benmhughes97@gmail.com>
2026-09-04 10:28:59 -0400

    Increase combat engagement ranges

--- js/weapons.js
+++ js/weapons.js
@@ -1,11 +1,11 @@
 export const WEAPONS={
-  rifle:{id:'rifle',name:'ASSAULT RIFLE',short:'RIFLE',damage:24,range:560,cooldown:.32,magazine:24,reload:1.35,accuracy:0,pellets:1,role:'assault',pressure:5},
-  pistol:{id:'pistol',name:'SIDEARM',short:'PISTOL',damage:16,range:380,cooldown:.42,magazine:15,reload:1.05,accuracy:2,pellets:1,role:'backup',pressure:2},
-  shotgun:{id:'shotgun',name:'BREACH SHOTGUN',short:'SHOTGUN',damage:13,range:300,cooldown:.9,magazine:6,reload:1.65,accuracy:-4,pellets:7,role:'breach',pressure:7},
-  sniper:{id:'sniper',name:'PRECISION RIFLE',short:'SNIPER',damage:72,range:900,cooldown:1.65,magazine:5,reload:1.8,accuracy:8,pellets:1,role:'precision',pressure:8},
-  lmg:{id:'lmg',name:'LIGHT MACHINE GUN',short:'LMG',damage:16,range:620,cooldown:.19,magazine:48,reload:2.15,accuracy:-7,pellets:1,role:'support',pressure:12},
-  dmr:{id:'dmr',name:'DESIGNATED MARKSMAN RIFLE',short:'DMR',damage:38,range:760,cooldown:.68,magazine:12,reload:1.55,accuracy:6,pellets:1,role:'marksman',pressure:6},
-  smg:{id:'smg',name:'SUBMACHINE GUN',short:'SMG',damage:18,range:430,cooldown:.15,magazine:32,reload:1.25,accuracy:-6,pellets:1,role:'flanker',pressure:4}
+  rifle:{id:'rifle',name:'ASSAULT RIFLE',short:'RIFLE',damage:24,range:760,cooldown:.32,magazine:24,reload:1.35,accuracy:0,pellets:1,role:'assault',pressure:5},
+  pistol:{id:'pistol',name:'SIDEARM',short:'PISTOL',damage:16,range:520,cooldown:.42,magazine:15,reload:1.05,accuracy:2,pellets:1,role:'backup',pressure:2},
+  shotgun:{id:'shotgun',name:'BREACH SHOTGUN',short:'SHOTGUN',damage:13,range:440,cooldown:.9,magazine:6,reload:1.65,accuracy:-4,pellets:7,role:'breach',pressure:7},
+  sniper:{id:'sniper',name:'PRECISION RIFLE',short:'SNIPER',damage:72,range:1200,cooldown:1.65,magazine:5,reload:1.8,accuracy:8,pellets:1,role:'precision',pressure:8},
+  lmg:{id:'lmg',name:'LIGHT MACHINE GUN',short:'LMG',damage:16,range:860,cooldown:.19,magazine:48,reload:2.15,accuracy:-7,pellets:1,role:'support',pressure:12},
+  dmr:{id:'dmr',name:'DESIGNATED MARKSMAN RIFLE',short:'DMR',damage:38,range:1020,cooldown:.68,magazine:12,reload:1.55,accuracy:6,pellets:1,role:'marksman',pressure:6},
+  smg:{id:'smg',name:'SUBMACHINE GUN',short:'SMG',damage:18,range:620,cooldown:.15,magazine:32,reload:1.25,accuracy:-6,pellets:1,role:'flanker',pressure:4}
 };
 export function weaponCopy(id){const w=WEAPONS[id]||WEAPONS.rifle;return Object.assign({},w,{ammo:w.magazine,recoil:0,fireCooldown:0})}
 export function getWeapon(id){return WEAPONS[id]||WEAPONS.rifle}
