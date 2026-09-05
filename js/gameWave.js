3d5cd223ef7f42d095f50998f6c5d850c6454a46
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 01:27:51 -0400

    Load depth masked blood layer

--- js/gameWave.js
+++ js/gameWave.js
@@ -1,5 +1,5 @@
 import './squadHud.js?v=20260905-50';
 import './playerAggression.js?v=20260905-52';
 import './combatHud.js?v=20260905-53';
-import './bloodOverlay.js?v=20260905-53';
+import './bloodOverlay.js?v=20260905-54';
 export * from './game.js?v=20260905-50';
