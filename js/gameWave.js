4b200973ffb9e4856ba70ccdb9c4d44322563007
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 02:02:39 -0400

    Refresh battle modules for monster sprite build

--- js/gameWave.js
+++ js/gameWave.js
@@ -1,7 +1,7 @@
-import './squadHud.js?v=20260905-50';
-import './autoplayTracers.js?v=20260905-55';
-import './playerAggression.js?v=20260905-55';
-import './combatHud.js?v=20260905-53';
-import './bloodOverlay.js?v=20260905-54';
-import './squadDrawer.js?v=20260905-56';
-export * from './game.js?v=20260905-50';
+import './squadHud.js?v=20260905-58';
+import './autoplayTracers.js?v=20260905-58';
+import './playerAggression.js?v=20260905-58';
+import './combatHud.js?v=20260905-58';
+import './bloodOverlay.js?v=20260905-58';
+import './squadDrawer.js?v=20260905-58';
+export * from './game.js?v=20260905-58';
