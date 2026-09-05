b311e88e0be688a2158934836f26adf0a4ecbdce
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 00:32:41 -0400

    Load ally defense stats

--- js/ally.js
+++ js/ally.js
@@ -1,4 +1,4 @@
-import { createAllies, updateAllies as updateAlliesCore, SQUAD_MODES } from './allyCore2.js?v=20260905-47';
+import { createAllies, updateAllies as updateAlliesCore, SQUAD_MODES } from './allyCore2.js?v=20260905-49';
 import { drawSoldier } from './soldierAssets.js?v=20260905-47';
 export { createAllies, SQUAD_MODES };
 var ALLY_LINES={contact:['CONTACT!','ENEMY SPOTTED!','I SEE THEM!','EYES UP!'],fire:['ENGAGING!','SENDING ROUNDS!','KEEP THEIR HEADS DOWN!','ON TARGET!'],cover:['I\'M SET!','HOLDING HERE!','COVERING!','GOOD POSITION!'],move:['MOVING!','COVER ME!','PUSHING UP!','REPOSITIONING!'],danger:['TAKING FIRE!','I\'M PINNED!','GET DOWN!','ROUNDS INCOMING!'],kill:['TARGET DOWN!','ONE DOWN!','GOT ONE!','HOSTILE DOWN!'],calm:['STAY SHARP.','WATCH YOUR SECTORS.','CHECK AMMO.','STAY WITH ME.']};
