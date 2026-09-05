0279b293e8962896f6e4de670cb31153d2373d11
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 01:36:34 -0400

    Load collapsible squad drawer

--- js/gameWave.js
+++ js/gameWave.js
@@ -3,4 +3,5 @@ import './autoplayTracers.js?v=20260905-55';
 import './playerAggression.js?v=20260905-55';
 import './combatHud.js?v=20260905-53';
 import './bloodOverlay.js?v=20260905-54';
+import './squadDrawer.js?v=20260905-56';
 export * from './game.js?v=20260905-50';
