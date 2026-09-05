841e2966b6aa4372d22f9563282a7f82b8b3187c
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 01:32:03 -0400

    Load autoplay tracer renderer

--- js/gameWave.js
+++ js/gameWave.js
@@ -1,5 +1,6 @@
 import './squadHud.js?v=20260905-50';
-import './playerAggression.js?v=20260905-52';
+import './autoplayTracers.js?v=20260905-55';
+import './playerAggression.js?v=20260905-55';
 import './combatHud.js?v=20260905-53';
 import './bloodOverlay.js?v=20260905-54';
 export * from './game.js?v=20260905-50';
