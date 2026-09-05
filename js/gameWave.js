7f8f88d9c55979f76c4bacad452771d5dc1c1341
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 01:24:12 -0400

    Load combat HUD and blood effects

--- js/gameWave.js
+++ js/gameWave.js
@@ -1,3 +1,5 @@
 import './squadHud.js?v=20260905-50';
-import './playerAggression.js?v=20260905-51';
+import './playerAggression.js?v=20260905-52';
+import './combatHud.js?v=20260905-53';
+import './bloodOverlay.js?v=20260905-53';
 export * from './game.js?v=20260905-50';
