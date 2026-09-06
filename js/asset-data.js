798519bb19cbb4db0f3f28eabc4179e0f4fc69ee
StormRat115 <Benmhughes97@gmail.com>
2026-09-05 22:10:22 -0400

    Add CSS, cover module, asset-data loader

---
+++ js/asset-data.js
@@ -0,0 +1,5 @@
+import tileAsphalt from "./asset-tileAsphalt.js";
+import ruinA from "./asset-ruinA.js";
+import ruinB from "./asset-ruinB.js";
+import jersey from "./asset-jersey.js";
+export const assetData = { tileAsphalt, ruinA, ruinB, jersey };
